ad1cd71b97f9c04c61823ea25583dd06c0c4a26e
Muhammad Usman <muhammadusman319320@gmail.com>
2023-10-13 01:18:06 +0500

    Animations Added

--- Rectangle.py
+++ Rectangle.py
@@ -6,3 +6,6 @@ class Test(Scene):
         rectangle2 = Rectangle().shift(DOWN * 2)
 
         self.add(rectangle1, rectangle2)
+        self.play(rectangle1.animate.set_fill(color= [TEAL, GREEN], opacity= 1), run_time=2)
+        self.play(rectangle2.animate.scale(1.2), run_time=2)
+        self.wait(1)
