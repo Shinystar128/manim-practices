50e0c441d42dfed54a40bb368917b7c74f62a31c
Shinystar128 <115920405+Shinystar128@users.noreply.github.com>
2024-01-01 04:53:18 +0500

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -8,4 +8,4 @@ class Test(Scene):
         self.add(rectangle1, rectangle2)
         self.play(rectangle1.animate.set_fill(color= [TEAL, GREEN], opacity= 1), run_time=2)
         self.play(rectangle2.animate.scale(1.2), run_time=2)
-        self.wait(1)
+        self.wait(2)
